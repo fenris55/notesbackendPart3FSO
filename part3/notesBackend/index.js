fea081f35010384ba9365d3fcbfdf4a50a531289
Rebecca Biancofiore <rebeccabiancofiore79@gmail.comm>
2024-08-01 10:29:35 -0400

    refactor

--- part3/notesBackend/index.js
+++ part3/notesBackend/index.js
@@ -55,8 +55,8 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({error: 'content missing'})
+  if (!body.name) {
+    return response.status(400).json({error: 'name missing'})
   }
 
   const note = {
